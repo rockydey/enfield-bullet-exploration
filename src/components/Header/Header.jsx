3b5193a8eb2615ea1d90d53746187227880c241c
rockydey <coderrocky25@gmail.com>
2022-04-03 23:35:41 +0600

    Home page head section complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <nav className='flex justify-center mt-3 mb-5'>
+            <nav className='flex justify-center mt-5 mb-20'>
                 <CustomLink className='mr-7 uppercase text-xl font-semibold' to='/home'>Home</CustomLink>
                 <CustomLink className='mr-7 uppercase text-xl font-semibold' to='/review'>Review</CustomLink>
                 <CustomLink className='mr-7 uppercase text-xl font-semibold' to='/dashboard'>Dashboard</CustomLink>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Home.css';
+import bullet from '../../image/bullet.png'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1>This is Home.</h1>
+        <div className='home flex justify-between mx-20'>
+            <div className='flex flex-col justify-center'>
+                <h1 className='text-5xl font-bold uppercase leading-none'>Best Ride With<br /><span className='text-green-600'>Enfield Bullet 360</span></h1>
+                <p className='w-11/12 text-lg my-4 text-gray-600'>The Royal Enfield Bullet 360 is one of the 7 different products from Royal Enfield on sale in India. It has a starting price of $4320 and comes in 2 different versions. The Royal Enfield Bullet 360 generates a max power of 19.1 bhp @ 5250 rpm along with a peak torque of 28 Nm @ 4000 rpm and the engine is mated to a 5 Speed speed gearbox.</p>
+                <button className='w-32 rounded-md px-5 py-3 bg-green-600 text-white font-bold' onClick={() => navigate('/')}>Live Demo</button>
+            </div>
+            <div className='img-div'>
+                <img src={bullet} alt="bullet" />
+            </div>
         </div>
     );
 };
