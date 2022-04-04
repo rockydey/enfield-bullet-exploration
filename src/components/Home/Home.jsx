364911c5b450dd9f5c4ff8acbd6cd4414031375a
rockydey <coderrocky25@gmail.com>
2022-04-04 10:47:09 +0600

    home component head section paragraph update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <section className='head-section md:block lg:flex justify-between md:mx-20 mb-20'>
                 <div className='flex flex-col justify-center'>
                     <h1 className='text-5xl font-bold uppercase leading-none'>Best Ride With<br /><span className='text-green-600'>Enfield Bullet 360</span></h1>
-                    <p className='w-11/12 text-lg my-4 text-gray-600'>The Royal Enfield Bullet 360 is one of the 7 different products from Royal Enfield on sale in India. It has a starting price of $4320 and comes in 2 different versions. The Royal Enfield Bullet 360 generates a max power of 19.1 bhp @ 5250 rpm along with a peak torque of 28 Nm @ 4000 rpm and the engine is mated to a 5 Speed speed gearbox.</p>
+                    <p className='w-11/12 text-lg my-4 text-gray-600'>The Royal Enfield Bullet 360 is one of the 7 different products from Royal Enfield on sale in Bangladesh. It has a starting price of Rs 421000 and comes in 2 different versions. The Royal Enfield Bullet 360 generates a max power of 19.1 bhp @ 5250 rpm along with a peak torque of 28 Nm @ 4000 rpm and the engine is mated to a 5 Speed speed gearbox.</p>
                     <button className='w-32 rounded-md px-5 py-3 bg-green-600 text-white font-bold' onClick={() => navigate('/')}>Live Demo</button>
                 </div>
                 <div className='img-div'>
