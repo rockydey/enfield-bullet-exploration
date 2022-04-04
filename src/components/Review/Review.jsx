966ab316dc138159dd7d2c362becd7d94b367145
rockydey <coderrocky25@gmail.com>
2022-04-04 18:42:56 +0600

    review component update

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -6,13 +6,16 @@ import './Review.css';
 const Review = () => {
     const [reviews, setReviews] = useReviews();
     return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-7 md:mx-20'>
-            {
-                reviews.map(review => <ReviewBullet
-                    key={review.id}
-                    review={review}
-                ></ReviewBullet>)
-            }
+        <div>
+            <h1 className='text-center mb-20 text-3xl font-bold'>What Our Customers Say!</h1>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-7 md:mx-20'>
+                {
+                    reviews.map(review => <ReviewBullet
+                        key={review.id}
+                        review={review}
+                    ></ReviewBullet>)
+                }
+            </div>
         </div>
     );
 };
